f65880dc80c9dcc6236980d36deb117fb0f2a3e2
Gina Häußge <osd@foosel.net>
2015-01-27 12:25:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Plugin Skeleton
+===============
+
+This is a basic plugin skeleton that you can use as a basis for your own OctoPrint plugin.
+
+You can copy the files to a folder of your choice, or just fork this repository, renaming it in the process, and check
+it out. The modify ``setup.py`` to fit your plugin, rename ``octoprint_skeleton`` accordingly and finally implement
+your plugin under ``octoprint_<plugin identifier>``.
---
+++ octoprint_skeleton/__init__.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started.
+
+import octoprint.plugin
+
+class SkeletonPlugin(octoprint.plugin.TemplatePlugin):
+	# TODO Implement me!
+	pass
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "Plugin Skeleton"
+__plugin_implementations__ = [SkeletonPlugin()]
