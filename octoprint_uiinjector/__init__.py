4a7b69b276b3e4829de625d8bf3fb3db017d8036
Kragrathea <maildropdrop@gmail.com>
2019-04-06 22:25:37 -0700

    JS

--- octoprint_uiinjector/__init__.py
+++ octoprint_uiinjector/__init__.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class UIInjectorPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
         self._logger.info("UI Injector Started.")
     pass
---
+++ octoprint_uiinjector/static/js/uiinjector.js
@@ -0,0 +1,3 @@
+$(function () {
+    alert("UI Injector");
+}
