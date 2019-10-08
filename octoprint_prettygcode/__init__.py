f243c839c07e70a818089ab75ba3a280fdd090bc
Kragrathea <maildropdrop@gmail.com>
2019-10-08 13:02:13 -0700

    Preparing for release

--- octoprint_prettygcode/__init__.py
+++ octoprint_prettygcode/__init__.py
@@ -17,8 +17,30 @@ class PrettyGCodePlugin(octoprint.plugin.StartupPlugin,
                 "js/LineMaterial.js","js/LineSegments2.js","js/Line2.js","js/camera-controls.js","js/Lut.js","js/dat.gui.min.js"],
             css=["css/prettygcode.css"]
         )
+
+            ##~~ Softwareupdate hook
+    def get_update_information(self):
+        return dict(
+            prettygcode=dict(
+                displayName="PrettyGCode",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="Kragrathea",
+                repo="OctoPrint-PrettyGCode",
+                current=self._plugin_version,
+                # update method: pip
+                pip="https://github.com/Kragrathea/OctoPrint-PrettyGCode/archive/{target_version}.zip"
+            )
+        )
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "PrettyGCode"
-__plugin_implementation__ = PrettyGCodePlugin()
+    global __plugin_implementation__ = PrettyGCodePlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
