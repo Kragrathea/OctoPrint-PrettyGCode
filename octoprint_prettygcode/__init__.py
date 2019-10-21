2b8aeb7e477e24d4cda921d839a1d20b6dc0c3ec
Kragrathea <maildropdrop@gmail.com>
2019-10-20 21:43:04 -0700

    Fix typo

--- octoprint_prettygcode/__init__.py
+++ octoprint_prettygcode/__init__.py
@@ -38,9 +38,11 @@ class PrettyGCodePlugin(octoprint.plugin.StartupPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "PrettyGCode"
+def __plugin_load__():
     global __plugin_implementation__ = PrettyGCodePlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
+
