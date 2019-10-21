894c0eba6336c9ba3dbe45e265632bdc081eb1ff
Kragrathea <maildropdrop@gmail.com>
2019-10-20 21:49:38 -0700

    Python is lame

--- octoprint_prettygcode/__init__.py
+++ octoprint_prettygcode/__init__.py
@@ -39,7 +39,8 @@ class PrettyGCodePlugin(octoprint.plugin.StartupPlugin,
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "PrettyGCode"
 def __plugin_load__():
-    global __plugin_implementation__ = PrettyGCodePlugin()
+    global __plugin_implementation__ 
+    __plugin_implementation__ = PrettyGCodePlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
